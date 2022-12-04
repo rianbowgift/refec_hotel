a3eb1fff6a294f26c31e35c31e994d6a22c37e46
rianbowgift <48279678+rianbowgift@users.noreply.github.com>
2022-12-05 00:05:20 +0900

    Delete Downloads/refec_hotel directory

--- Downloads/refec_hotel/src/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import entity.Customer;
-
-public interface LoginService {
-    public int customerSignInOrAdminSignIn(String id, String password);
-    public void SignUp(String id,String password,String name,String number);
-    public void findCustomerInformation();
-}
